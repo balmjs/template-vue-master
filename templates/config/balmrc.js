fd333d7990e70c6caa1637f34ab77ae154dc5ec4
Elf-mousE <ifmouz@gmail.com>
2020-04-20 10:37:12 +0800

    update

--- templates/config/balmrc.js
+++ templates/config/balmrc.js
@@ -6,14 +6,13 @@ const VueLoaderPlugin = require('vue-loader/lib/plugin');
 // 中文文档 - https://balmjs.com/docs/v2/zh/config/
 module.exports = {
   server: {
-    open: true,
-    proxyConfig: {
-      context: '/api',
-      options: {
-        target: 'http://your.project.dev', // Target host
-        changeOrigin: true // Needed for virtual hosted sites
-      }
-    },
+    // proxyConfig: {
+    //   context: '/api',
+    //   options: {
+    //     target: 'http://your.project.dev', // Target host
+    //     changeOrigin: true // Needed for virtual hosted sites
+    //   }
+    // },
     historyOptions: true // For vue-router `mode: 'history'`
   },
   roots: {
--- templates/gulpfile.js
+++ templates/gulpfile.js
@@ -1,7 +1,7 @@
 /* eslint-env node */
 const balm = require('balm');
 const config = require('./config/balmrc');
-const publish = require('./config/publish');
+const publish = require('./config/balm.publish');
 
 balm.config = config;
 
